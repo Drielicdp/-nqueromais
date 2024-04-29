d327f9cf37f530cdae37bf8ca004d0b8c406da2c
Drielicdp <130789921+Drielicdp@users.noreply.github.com>
2024-04-29 09:54:21 -0300

    main.js

--- main.js
+++ main.js
@@ -89,3 +89,16 @@ function comecaCronometro(){
 }
 
 comecaCronometro();
+}
+
+function atualizaCronometro(){
+
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);   
+    }
+@@ -52,4 +53,4 @@ function comecaCronometro(){
+    setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
+//comecaCronometro();
